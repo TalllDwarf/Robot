92bde0325d48148d5d08cca7d341f9b9361d4ec8
M.A.V.Booth U1552351 <U1552351@unimail.hud.ac.uk>
2017-10-30 17:56:53 +0000

    Init Push

---
+++ Source/Robot/RobotGameMode.cpp
@@ -0,0 +1,15 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#include "RobotGameMode.h"
+#include "RobotCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+ARobotGameMode::ARobotGameMode()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
+	if (PlayerPawnBPClass.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+}

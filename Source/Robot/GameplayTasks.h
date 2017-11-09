1a888c8def34a7e7e6d8fbfdd0eef692b32df6cc
Jacktivity <jh140695@gmail.com>
2017-11-09 14:36:15 +0000

    AI Base
    Finds and follows player

---
+++ Source/Robot/GameplayTasks.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTasksComponent.h"
+#include "GameplayTasks.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ROBOT_API UGameplayTasks : public UGameplayTasksComponent
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
